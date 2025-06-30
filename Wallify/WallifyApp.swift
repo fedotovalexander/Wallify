0fd67d3094885221123c31f94e2f3e86f526e640
fedotovalexander <fedotov.sanek@gmail.com>
2025-06-30 14:31:16 +0300

    Initial Commit

---
+++ Wallify/WallifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallifyApp.swift
+//  Wallify
+//
+//  Created by Alexander Fedotov on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WallifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
